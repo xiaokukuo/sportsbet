f9df6e262ce83346aca471bec7305256ed1e8bc6
xiaokukuo <976886537@qq.com>
2018-07-18 21:05:43 +0800

    111

--- sport-parent/sport-common/src/main/java/com/sport/bet/common/structure/stack/utils/ExpressionStack2.java
+++ sport-parent/sport-common/src/main/java/com/sport/bet/common/structure/stack/utils/ExpressionStack2.java
@@ -1,4 +1,4 @@
-/*package com.sport.bet.common.structure.stack.utils;
+package com.sport.bet.common.structure.stack.utils;
 
 import com.sport.bet.common.structure.stack.LinkStack;
 
@@ -8,57 +8,54 @@ public class ExpressionStack2<E> {
 	LinkStack<Character> operatorStack = new LinkStack<Character>();// 存放运算符的栈
 
 	@SuppressWarnings("unchecked")
-	public E evaluateExpression(String expression) {
+	public String evaluateExpression(String expression) {
 		// 将算术表达式转换为后缀表达式的函数，结果一字符串的形式返回
 		String postfix = new String(); // 用于储存后缀表达式
 		for (int i = 0; expression != null && i < expression.length(); i++) {
 			char c = expression.charAt(i);
 			if (' ' != c) {
 				// 为左括号
-				if (isOpenParent(c)) {
-					st.push(c);
-				} // 为右括号
-				else if (isCloseParent(c)) {
-					char ac = st.pop();
-					while (!isOpenParent(ac)) {
+				if (isOpenParenthesis(c)) {
+					operatorStack.push(c);
+				} else if (isCloseParenthesis(c)) {  // 为右括号
+					char ac = operatorStack.pop();
+					while (!isOpenParenthesis(ac)) {
 						postfix = postfix.concat(String.valueOf(ac));
-						ac = st.pop();
+						ac = operatorStack.pop();
 					}
-				} // 为运算符
-				else if (isOperator(c)) {
+				} else if (isOperator(c)) {  // 为运算符
 					// 运算栈非空，取出栈顶优先级高的运算符送完后缀表达式
-					if (!st.empty()) {
-						char ac = st.pop();
+					if (!operatorStack.isEmpty()) {
+						char ac = operatorStack.pop();
 						// 栈取出的字符优先级比c高
-						while (!st.isEmpty() && priority(ac) >= priority(c)) {
+						while (!operatorStack.isEmpty() && priority(ac) >= priority(c)) {
 							postfix = postfix.concat(String.valueOf(ac));
-							ac = st.pop();
+							ac = operatorStack.pop();
 						} // 栈取出的字符优先级比c低，则将取出的字符重新入栈
 						if (ac != ' ') {
-							st.push(ac);
+							operatorStack.push(ac);
 						}
 					}
-					st.push(c); // 将c入栈
-				} // 为操作数，直接串联到postfix内
-				else {
+					operatorStack.push(c); // 将c入栈
+				} else { // 为操作数，直接串联到postfix内
 					postfix = postfix.concat(String.valueOf(c));
 				}
 			}
 		} // 当表达式读完就将算术栈pop出加入postfix
-		while (!st.isEmpty()) {
-			postfix = postfix.concat(String.valueOf(st.pop()));
+		while (!operatorStack.isEmpty()) {
+			postfix = postfix.concat(String.valueOf(operatorStack.pop()));
 
 		}
 		return postfix;
 
 	}
 
-	*//**
+	/**
 	 * 对操作符栈顶的一个操作符进行计算
 	 * 
 	 * @param operandStack
 	 * @param operatorStack
-	 *//*
+	 */
 	@SuppressWarnings("unchecked")
 	public void processOneOperator() {
 		char op = operatorStack.pop(); // 取操作符的栈顶元素
@@ -122,4 +119,3 @@ public class ExpressionStack2<E> {
 	}
 
 }
-*/
